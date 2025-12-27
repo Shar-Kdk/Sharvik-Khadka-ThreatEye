2b2ca07dd29b4cd9beaba12fa0b1f24fccb666b9
Shar-Kdk <np05cp4a230249@iic.edu.np>
2025-12-27 23:26:39 +0545

    chore: cleanup project structure, migrate from SQLite to MySQL, downgrade Django to 4

--- Backend/ThreatEye/settings.py
+++ Backend/ThreatEye/settings.py
@@ -77,10 +77,17 @@ WSGI_APPLICATION = 'ThreatEye.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'threateye',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
